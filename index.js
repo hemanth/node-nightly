259f24994fcc41b7716152ce9e6f1920e41730fa
Hemanth.HM <hemanth.hm@gmail.com>
2016-06-26 17:03:49 +0530

    using node-nightly-version module

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 'use strict';
-var fetch = require('isomorphic-fetch');
-var download = require('download');
-var mv = require('fs').rename;
+const fetch = require('isomorphic-fetch');
+const download = require('download');
+const nodeNightlyVersion = require('node-nightly-version');
+const mv = require('fs').rename;
 
 module.exports = {
   install: () => {
       console.log('Downloading the nightly version, hang on...');
-      fetch('https://nodejs.org/download/nightly/index.tab')
-      .then(resp => resp.text())
-      .then(text => {
-        var latest = text.split('\n')[1].split('\t')[0];
-        var os = process.platform;
-        var arch = process.arch;
-        var type = 'nightly';
-        var url = `https://nodejs.org/download/${type}/${latest}/node-${latest}-${os}-${arch}.tar`
+       nodeNightlyVersion().then(latest => {
+        const os = process.platform;
+        const arch = process.arch;
+        const type = 'nightly';
+        const url = `https://nodejs.org/download/${type}/${latest}/node-${latest}-${os}-${arch}.tar`
         download(url, './', {extract:true})
         .then( _ => {
           mv(`./node-${latest}-${os}-${arch}`, `./node-nightly`)
           console.log('node-nightly is avalible on CLI! Just try `node-nightly`')
-        })
-        .catch(err => console.error(err));
-      })
-    .catch(err => console.error(err))
+        }).catch(err => console.error(err));
+      }).catch(err => console.error(err))
   }
 };
