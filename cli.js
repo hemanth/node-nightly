652b335a58125907d2b1e99500f174047b9e78f1
Hemanth.HM <hemanth.hm@gmail.com>
2016-06-27 18:25:07 +0530

    Notify and update on nightly + gif 🤓

--- cli.js
+++ cli.js
@@ -1,8 +1,37 @@
 #!/usr/bin/env node
 'use strict';
-var kexec = require('kexec');
-var nodeNightly = require('./');
-var existsSync = require('fs').existsSync
-var args = process.argv.slice(2);
+const kexec = require('kexec');
+const nodeNightly = require('./');
+const existsSync = require('fs').existsSync;
+const execFileSync = require('child_process').execFileSync
+const Configstore = require('configstore');
+const pkg = require('./package.json');
+const prompt = require('single-prompt').prompt;
+const nodeNightlyVersion = require('node-nightly-version');
 
-!existsSync('./node-nightly') ? nodeNightly.install() : kexec('./node-nightly/bin/node', args)
+const args = process.argv.slice(2);
+const extractDate = versionString => ~~versionString.split('nightly')[1].slice(0,8);
+const compVersion = (currentVersion, latestVersion) => extractDate(currentVersion) < extractDate(latestVersion);
+
+if(!existsSync('./node-nightly')) {
+  console.log('Downloading the nightly version, hang on...');
+  nodeNightly.install();
+} else {
+  nodeNightlyVersion().then(latestVersion => {
+      const currentVersion = new Configstore(pkg.name).get('version');
+      if(compVersion(currentVersion, latestVersion)) {
+          prompt('Newer version is avaible, do you want to upgrade?', ['y', 'n'])
+              .then(function(choice) {
+              if(choice === 'y') {
+                  console.log('Updating...')
+                  nodeNightly.update();
+              } else {
+                  kexec('./node-nightly/bin/node', args);
+              }
+          }).catch(console.error);
+      }
+      else {
+          kexec('./node-nightly/bin/node', args);
+      }
+  });
+}
--- index.js
+++ index.js
@@ -2,21 +2,32 @@
 const fetch = require('isomorphic-fetch');
 const download = require('download');
 const nodeNightlyVersion = require('node-nightly-version');
+const Configstore = require('configstore');
+const pkg = require('./package.json');
 const mv = require('fs').rename;
+const rm = require('rimraf');
 
 module.exports = {
   install: () => {
-      console.log('Downloading the nightly version, hang on...');
        nodeNightlyVersion().then(latest => {
+        const conf = new Configstore(pkg.name);
+        conf.set('version', latest);
         const os = process.platform;
         const arch = process.arch;
         const type = 'nightly';
         const url = `https://nodejs.org/download/${type}/${latest}/node-${latest}-${os}-${arch}.tar`
         download(url, './', {extract:true})
         .then( _ => {
-          mv(`./node-${latest}-${os}-${arch}`, `./node-nightly`)
-          console.log('node-nightly is avalible on CLI! Just try `node-nightly`')
+          mv(`./node-${latest}-${os}-${arch}`, `./node-nightly`);
+          console.log('node-nightly is avalible on your CLI! ');
+          process.exit(0);
         }).catch(err => console.error(err));
       }).catch(err => console.error(err))
+  },
+  update: function() {
+     console.log('Deleting old version');
+     rm.sync('./node-nightly');
+     console.log('Deleted, installing newer version..');
+     this.install();
   }
 };
