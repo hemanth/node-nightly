361cd61cea9be320a375eb332e29a1e4856cdca4
Hemanth.HM <hemanth.hm@gmail.com>
2016-06-28 21:46:45 +0530

    --upgrade flag and less noise, as per @paulirish's suggestion

--- cli.js
+++ cli.js
@@ -6,32 +6,29 @@ const existsSync = require('fs').existsSync;
 const execFileSync = require('child_process').execFileSync
 const Configstore = require('configstore');
 const pkg = require('./package.json');
-const prompt = require('single-prompt').prompt;
 const nodeNightlyVersion = require('node-nightly-version');
 
-const args = process.argv.slice(2);
 const extractDate = versionString => ~~versionString.split('nightly')[1].slice(0,8);
 const compVersion = (currentVersion, latestVersion) => extractDate(currentVersion) < extractDate(latestVersion);
+let args = process.argv.slice(2);
+
+// Check for upgrade.
+let index = args.indexOf('--upgrade');
+if(!!~index) {
+  args = args.splice(index, 1);
+  nodeNightly.update();
+}
 
 if(!existsSync('./node-nightly')) {
+  //First install
   console.log('Downloading the nightly version, hang on...');
   nodeNightly.install();
 } else {
   nodeNightlyVersion().then(latestVersion => {
       const currentVersion = new Configstore(pkg.name).get('version');
       if(compVersion(currentVersion, latestVersion)) {
-          prompt('Newer version is avaible, do you want to upgrade?', ['y', 'n'])
-              .then(function(choice) {
-              if(choice === 'y') {
-                  console.log('Updating...')
-                  nodeNightly.update();
-              } else {
-                  kexec('./node-nightly/bin/node', args);
-              }
-          }).catch(console.error);
+        console.log('\x1b[36m', 'New nightly available. To upgrade: `node-nightly --upgrade`' ,'\x1b[0m');
+        kexec('./node-nightly/bin/node', args);
       }
-      else {
-          kexec('./node-nightly/bin/node', args);
-      }
-  });
+    }).catch(console.error);
 }
