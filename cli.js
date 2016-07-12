4cb149f1aef80d82a6a2c48b9466e0b4fe1dc25a
tarungarg546 <tarungarg546@gmail.com>
2016-07-13 04:01:00 +0530

    Make it work on windows with some tweaks
    Updates :-
       1. Some node specific tweaks for windows like
           1.1 Using .zip instead of .tar.gz if it's windows
           1.2 Using os as 'win' instead of 'win32' which is returned by process.platform
           1.3 Executing node-nightly binary from /node-nightly/node on
           windows
       2. Using cross-spawn.

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
-const kexec = require('kexec');
+
+const spawn = require('cross-spawn').spawn;
 const nodeNightly = require('./');
 const existsSync = require('graceful-fs').existsSync;
 
@@ -9,21 +10,38 @@ let args = process.argv.slice(2);
 // Check for upgrade.
 let index = args.indexOf('--upgrade');
 if(!!~index) {
+
 	nodeNightly.update().then(res => {
 		if(res !== 'Installed') {
 			console.log(res);
 		}
 		process.exit(0);
-	}).catch(console.error);
+	})
+	.catch(console.error);
+
 } else if(!existsSync(`${__dirname}/node-nightly`)) {
 	//First install
 	console.log('Downloading the nightly version, hang on...');
 	nodeNightly.install().catch(console.error);
+
 } else {
-	nodeNightly.check().then(updatedVersion => {
-		if(updatedVersion) {
+
+	nodeNightly.check().then(isUpdateAvailable => {
+
+		if(isUpdateAvailable) {
 			console.log('\x1b[36m', 'New nightly available. To upgrade: `node-nightly --upgrade`' ,'\x1b[0m');
 		}
-		kexec(`${__dirname}/node-nightly/bin/node`, args);
-	}).catch(console.error);
+
+		let path;
+		if(/^win/.test(process.platform)) {
+			//if it's a windows platform
+			path = `${__dirname}/node-nightly/node.exe`;
+		} else {
+			//other than windows
+			path = `${__dirname}/node-nightly/bin/node`;
+		}
+		spawn(path, args,  {stdio: 'inherit', env: process.env});
+
+	})
+	.catch(console.error);
 }
