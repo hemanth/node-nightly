fb48824c07d77eadd7b9c8415ecedfc3411c1977
Tarun Garg <tarungarg546@gmail.com>
2016-07-14 09:38:50 +0530

    Update in how install and update works together (#15)
    * Update only when user's node-nightly is outdated
    
    * typo fix
    
    * Move compVersion and extractDate to index.js from cli.js
    
    * Remove pkg and configStore from cli.js
    
    * Improved installation and upgradation phase
    
    Now in this iteration when we are checking if
    update is there for node-nightly and if there is
    update we are calling install again and in that install we were again calling
    nodeNightlyVersion() which was un-neccessary because we had that already when
    checking for update so now, we are using that value in install phase and if that is
    not present we are going to nodeNightlyVersion()
    
    * :ant: :lollipop:
    
    * Indentation fixes + unhandled promises catcj
    
    * Removed repeated code
    
    * Moving downloaded node-nightly in both upgrade and install phase
    
    Previously it was only after upgrade
    By previously i mean after Rewrite Phase 1
    
    * Promise bug Removed from --upgrade and install cycle
    
    Previously we have something like nodeNightly.update().then(Process.exit(0))
    although it may look fine at first but acytually its a bug since
    .then() method of promise always expects a function to be there
    as a callback and if we do not provide it, then it skips the .then() part
    and move ahead so `process.exit(0)` in our case was not
    returning a function so yes this was a bug
    
    * Trying to fix ReferenceError
    
    * Remove flushing

--- cli.js
+++ cli.js
@@ -3,33 +3,27 @@
 const kexec = require('kexec');
 const nodeNightly = require('./');
 const existsSync = require('fs').existsSync;
-const Configstore = require('configstore');
-const pkg = require('./package.json');
-const nodeNightlyVersion = require('node-nightly-version');
 
-const extractDate = versionString => ~~versionString.split('nightly')[1].slice(0,8);
-const compVersion = (currentVersion, latestVersion) => extractDate(currentVersion) < extractDate(latestVersion);
 let args = process.argv.slice(2);
 
 // Check for upgrade.
 let index = args.indexOf('--upgrade');
 if(!!~index) {
-  args = args.splice(index, 1);
-  nodeNightly.update().then(_ => {
-    mv(`${__dirname}/node-${osArchString}`, `${__dirname}/node-nightly`);
-    console.log('node-nightly is available on your CLI! ');
-    process.exit(0);
-  })
+	nodeNightly.update().then(res => {
+		if(res !== 'Installed') {
+			console.log(res);
+		}
+		process.exit(0);
+	}).catch(console.error);
 } else if(!existsSync(`${__dirname}/node-nightly`)) {
-  //First install
-  console.log('Downloading the nightly version, hang on...');
-  nodeNightly.install();
+	//First install
+	console.log('Downloading the nightly version, hang on...');
+	nodeNightly.install().catch(console.error);
 } else {
-  nodeNightlyVersion().then(latestVersion => {
-      const currentVersion = new Configstore(pkg.name).get('version');
-      if(compVersion(currentVersion, latestVersion)) {
-        console.log('\x1b[36m', 'New nightly available. To upgrade: `node-nightly --upgrade`' ,'\x1b[0m');
-      }
-      kexec(`${__dirname}/node-nightly/bin/node`, args);
-    }).catch(console.error);
+	nodeNightly.check().then(updatedVersion => {
+		if(updatedVersion) {
+			console.log('\x1b[36m', 'New nightly available. To upgrade: `node-nightly --upgrade`' ,'\x1b[0m');
+		}
+		kexec(`${__dirname}/node-nightly/bin/node`, args);
+	}).catch(console.error);
 }
--- index.js
+++ index.js
@@ -3,27 +3,53 @@ const download = require('download');
 const nodeNightlyVersion = require('node-nightly-version');
 const Configstore = require('configstore');
 const pkg = require('./package.json');
-const mv = require('fs').rename;
 const rm = require('rimraf');
+const mv = require('fs').rename;
+
+const extractDate = versionString => ~~versionString.split('nightly')[1].slice(0,8);
+const compVersion = (currentVersion, latestVersion) => extractDate(currentVersion) < extractDate(latestVersion);
 
 module.exports = {
-  install: () => {
-  		let osArchString;
-      return nodeNightlyVersion().then(latest => {
-      	const conf = new Configstore(pkg.name);
-        conf.set('version', latest);
-        const os = process.platform;
-        const arch = process.arch;
-        const type = 'nightly';
-        osArchString = `${latest}-${os}-${arch}`;
-        const url = `https://nodejs.org/download/${type}/${latest}/node-${osArchString}.tar`;
-        return download(url, __dirname, {extract:true});
-      })
-  },
-  update: function() {
-    console.log('Deleting old version');
-    rm.sync('./node-nightly');
-    console.log(`Deleted!\nInstalling newer version..`);
-    return this.install();
-  }
+	install: (version) => {
+		let osArchString, nodeNightlyVer;
+		nodeNightlyVer = version !== undefined ? Promise.resolve(version) : nodeNightlyVersion();
+
+		return nodeNightlyVer.then(latest => {
+			const conf = new Configstore(pkg.name);
+			conf.set('version', latest);
+			const os = process.platform;
+			const arch = process.arch;
+			const type = 'nightly';
+			osArchString = `${latest}-${os}-${arch}`;
+			const url = `https://nodejs.org/download/${type}/${latest}/node-${osArchString}.tar.gz`;
+			return download(url, __dirname, {extract:true});
+		}).then(_ => {
+			mv(`${__dirname}/node-${osArchString}`, `${__dirname}/node-nightly`);
+			console.log(`node-nightly is available on your CLI!`);
+			return 'Installed';
+		})
+	},
+	update: function() {
+		console.log('Checking for update...');
+		return this.check().then(updatedVersion => {
+			if(updatedVersion) {
+				//update found
+				console.log('Deleting old version');
+				rm.sync('./node-nightly');
+				console.log(`Deleted!\nInstalling newer version..`);
+				return this.install(updatedVersion);
+			}
+			return 'You are using latest version already.';
+		});
+	},
+	check: function() {
+		return nodeNightlyVersion().then(latestVersion => {
+			const currentVersion = new Configstore(pkg.name).get('version');
+			if(compVersion(currentVersion, latestVersion)) {
+				return latestVersion;
+			}
+			return false;
+		});
+	}
 };
+
