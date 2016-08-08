fc278a3fb695a6935d69ac265f1ea7b39fbc3e10
Bhavesh Gohel <ibhaveshgohel@gmail.com>
2016-08-08 15:52:11 +0530

    Windows Compability Added!

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 const spawn = require('cross-spawn').spawn;
 const nodeNightly = require('./');
 const existsSync = require('graceful-fs').existsSync;
-
+const os = process.platform  === 'win32' ? 'win' : process.platform;
 let args = process.argv.slice(2);
 
 // Check for upgrade.
@@ -24,7 +24,10 @@ if(!!~index) {
 		if(updatedVersion) {
 			console.log('\x1b[36m', 'New nightly available. To upgrade: `node-nightly --upgrade`' ,'\x1b[0m');
 		}
-		//spawn(`${__dirname}/node-nightly/bin/node`, args, {stdio: 'inherit', env: process.env});
-		spawn(`${__dirname}/node-nightly/node`, args, {stdio: 'inherit', env: process.env}); //just for testing on windows
+		if(os === 'win'){
+			spawn(`${__dirname}/node-nightly/node`, args, {stdio: 'inherit', env: process.env});
+		} else{
+			spawn(`${__dirname}/node-nightly/bin/node`, args, {stdio: 'inherit', env: process.env});			
+		}
 	}).catch(console.error);
 }
