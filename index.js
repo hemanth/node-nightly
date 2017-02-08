2c3cbce02d226c9aad6ec282ca997dcc8dcf1d9a
Isaac Cambron <isaac@isaaccambron.com>
2017-02-08 01:55:27 -0500

    Add --version option and make more robust

--- index.js
+++ index.js
@@ -7,51 +7,65 @@ const rm = require('rimraf');
 const realFs = require('fs');
 const gracefulFs = require('graceful-fs');
 gracefulFs.gracefulify(realFs);
-const mv = gracefulFs.rename;
+const mv = gracefulFs.renameSync;
 
 const extractDate = versionString => ~~versionString.split('nightly')[1].slice(0, 8);
 const compVersion = (currentVersion, latestVersion) => extractDate(currentVersion) < extractDate(latestVersion);
 
 module.exports = {
+
 	install: (version) => {
 		let osArchString, nodeNightlyVer;
 		nodeNightlyVer = version !== undefined ? Promise.resolve(version) : nodeNightlyVersion();
 
 		return nodeNightlyVer.then(latest => {
-			const conf = new Configstore(pkg.name);
-			conf.set('version', latest);
-			const os = process.platform === 'win32' ? 'win' : process.platform;
-			const extention = os === 'win' ? 'zip' : 'tar.gz';
-			const arch = process.arch;
-			const type = 'nightly';
-			osArchString = `${latest}-${os}-${arch}`;
-			const url = `https://nodejs.org/download/${type}/${latest}/node-${osArchString}.${extention}`;
-			return download(url, __dirname, {
-				extract: true
-			});
-		}).then(_ => {
-			mv(`${__dirname}/node-${osArchString}`, `${__dirname}/node-nightly`);
-			console.log(`node-nightly is available on your CLI!`);
-			return 'Installed';
-		})
-	},
-	update: function() {
+      const os = process.platform === 'win32' ? 'win' : process.platform,
+            extention = os === 'win' ? 'zip' : 'tar.gz',
+            arch = process.arch,
+            type = 'nightly',
+            osArchString = `${version}-${os}-${arch}`,
+            url = `https://nodejs.org/download/${type}/${version}/node-${osArchString}.${extention}`,
+            nightlyDir = `${__dirname}/node-nightly`;
+
+      const extractedTo = `${__dirname}/node-${osArchString}`;
+
+      if (gracefulFs.existsSync(extractedTo)) {
+        rm.sync(extractedTo);
+      }
+
+      return download(url, __dirname, {
+        extract: true
+      }).then(_ => {
+        if (gracefulFs.existsSync(nightlyDir)) {
+          console.log('Deleting old version');
+          rm.sync('./node-nightly');
+          console.log(`Deleted!\nInstalling newer version..`);
+        }
+
+        mv(extractedTo, nightlyDir);
+        console.log(`node-nightly is available on your CLI!`);
+
+        new Configstore(pkg.name).set('version', version);
+
+        return 'Installed';
+      });
+    });
+  },
+
+  update: function() {
 		console.log('Checking for update...');
 		return this.check().then(updatedVersion => {
 			if (updatedVersion) {
-				//update found
-				console.log('Deleting old version');
-				rm.sync('./node-nightly');
-				console.log(`Deleted!\nInstalling newer version..`);
 				return this.install(updatedVersion);
 			}
 			return 'You are using latest version already.';
 		});
 	},
+
 	check: function() {
 		return nodeNightlyVersion().then(latestVersion => {
 			const currentVersion = new Configstore(pkg.name).get('version');
-			if (compVersion(currentVersion, latestVersion)) {
+			if (!currentVersion || compVersion(currentVersion, latestVersion)) {
 				return latestVersion;
 			}
 			return false;
